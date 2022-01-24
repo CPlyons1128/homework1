78fb0e14da194d09c1572ec78a0607f59ee0a7b8
CPlyons1128 <98299677+CPlyons1128@users.noreply.github.com>
2022-01-23 23:36:50 -0500

    Add files via upload
    HW1.java is what I currently have

---
+++ HW1.java
@@ -0,0 +1,25 @@
+/*
+ * Author:  Charles P. Lyons, clyons2020@my.fit.edu
+ * Class:  CSE 2010, Section E4
+ * Assignment = hw1S23Individual
+ * Charset: US-ASCII
+ */
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class HW1 {
+   public static void main (final String[] args) {
+
+      final Scanner sc = new Scanner(System.in);
+      // lists all seller's companies' names
+      LinkedList<String> sellerNames = new LinkedList<String>();
+
+      String inputType;
+
+      while (sc.hasNext()) {
+         inputType = sc.next();
+
+      }
+   }
+}
